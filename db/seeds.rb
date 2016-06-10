624990022905ab39c334bd0d7d89d0f72d6e4d13
Mark Richardson <markeban@yahoo.com>
2016-06-09 20:12:14 -0700

    finished indexing exerise

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-10000.times do
+1000000.times do
   Rsvp.create(
     :first_name => Faker::Name.first_name,
     :last_name => Faker::Name.last_name,
@@ -8,3 +8,12 @@
     :user_id => rand(1..1000)
     )
 end
+
+Rsvp.create(
+  :first_name => Faker::Name.first_name,
+  :last_name => Faker::Name.last_name,
+  :attending => true,
+  :vegetarian => false,
+  :guests => rand(0..7),
+  :user_id => 1001
+)
